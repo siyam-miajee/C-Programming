263db486b6e16edc4c3a773ca5303cbfc73a2bc8
Md. Siyam Miajee <146632996+siyam-miajee@users.noreply.github.com>
2026-09-03 18:23:47 +0600

    Create comments.c

---
+++ 01-Basics/comments.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+
+    // This is a single-line comment
+
+    /*
+       This is a
+       multi-line comment
+    */
+
+    printf("Comments are used to explain code.\n");
+
+    return 0;
+}
