808be47a8ad9c599a594054e9972e6e54f1c4a60
Md. Siyam Miajee <146632996+siyam-miajee@users.noreply.github.com>
2026-09-03 18:26:15 +0600

    Update comments.c

--- 01-Basics/comments.c
+++ 01-Basics/comments.c
@@ -1,16 +1,12 @@
 #include <stdio.h>
-
 int main()
 {
-
     // This is a single-line comment
 
     /*
        This is a
        multi-line comment
     */
-
     printf("Comments are used to explain code.\n");
-
     return 0;
 }
