017b95082c04094bbf90ca05338d5b333225a06d
Md. Siyam Miajee <146632996+siyam-miajee@users.noreply.github.com>
2026-09-04 08:12:38 +0600

    Create formatted_output.c

---
+++ 02-Input-Output/formatted_output.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int age = 20;
+    float cgpa = 3.75;
+    char grade = 'A';
+
+    printf("Age   : %d\n", age);
+    printf("CGPA  : %.2f\n", cgpa);
+    printf("Grade : %c\n", grade);
+    return 0;
+}
