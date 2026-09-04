86cadaaeb79dbd1a9f20a5a01a4d91a1de5ad879
Md. Siyam Miajee <146632996+siyam-miajee@users.noreply.github.com>
2026-09-04 07:46:15 +0600

    Create escape_sequences.c

---
+++ 01-Basics/escape_sequences.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    printf("Hello\nWorld\n");               // \n → New line
+    printf("Name:\tSiyam\n");               // \t → Tab
+    printf("He said, \"Hello!\"\n");        // \" → Double quote
+    printf("This is a backslash: \\\n");    // \\ → Backslash
+    return 0;
+}
