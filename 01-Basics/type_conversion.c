cfb0d9b45a787284d1afa30e3cc1d9e9bbbd2b47
Md. Siyam Miajee <146632996+siyam-miajee@users.noreply.github.com>
2026-09-04 07:21:03 +0600

    Create type_conversion.c

---
+++ 01-Basics/type_conversion.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int a = 10;
+    int b = 3;
+    float result = (float)a/b; //a converted int to float
+
+    printf("Result: %.2f\n", result);
+    return 0;
+}
