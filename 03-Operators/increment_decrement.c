82f7c75327c2eb066a7a00f5b88e142f0cd6b112
Md. Siyam Miajee <146632996+siyam-miajee@users.noreply.github.com>
2026-09-08 20:33:47 +0600

    Create increment_decrement.c

---
+++ 03-Operators/increment_decrement.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a = 5;
+
+    printf("Initial value: %d\n", a);
+    a++;
+    printf("After increment: %d\n", a);
+    a--;
+    printf("After decrement: %d\n", a);
+    return 0;
+}
