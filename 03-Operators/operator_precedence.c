16d2831a859d83e4232d1f0cf24b7bfeb6e3ce0f
Md. Siyam Miajee <146632996+siyam-miajee@users.noreply.github.com>
2026-09-08 20:42:04 +0600

    Create operator_precedence.c

---
+++ 03-Operators/operator_precedence.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int result;
+
+    result = 10 + 5 * 2;
+    printf("Result = %d\n", result);
+    result = (10 + 5) * 2;
+    printf("Result = %d\n", result);
+    return 0;
+}
