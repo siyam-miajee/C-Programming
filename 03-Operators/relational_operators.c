42ede38dbb4757b0891d41567cd1ad2914f6783f
Md. Siyam Miajee <146632996+siyam-miajee@users.noreply.github.com>
2026-09-08 20:15:30 +0600

    Create relational_operators.c

---
+++ 03-Operators/relational_operators.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a = 10;
+    int b = 5;
+    //Output- 1 = True, 0 = False
+    printf("a > b  : %d\n", a > b);
+    printf("a < b  : %d\n", a < b);
+    printf("a >= b : %d\n", a >= b);
+    printf("a <= b : %d\n", a <= b);
+    printf("a == b : %d\n", a == b);
+    printf("a != b : %d\n", a != b);
+    return 0;
+}
