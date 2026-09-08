aef857fa58edcded68850c6735183c6d79263053
Md. Siyam Miajee <146632996+siyam-miajee@users.noreply.github.com>
2026-09-08 20:49:18 +0600

    Update logical_operators.c

--- 03-Operators/logical_operators.c
+++ 03-Operators/logical_operators.c
@@ -3,9 +3,15 @@ int main()
 {
     int a = 10;
     int b = 5;
-    //Output- 1 = True, 0 = False
-    printf("(a > 5 && b < 10): %d\n", a > 5 && b < 10);
-    printf("(a > 5 || b > 10): %d\n", a > 5 || b > 10);
-    printf("!(a > 5): %d\n", !(a > 5));
+    
+    // 1 = True, 0 = False
+
+    /*&&  → AND → both conditions must be true
+    ||  → OR  → at least one condition must be true
+    !   → NOT → reverses true/false*/
+
+    printf("AND: %d\n", (a > 5) && (b < 10));
+    printf("OR: %d\n", (a > 5) || (b > 10));
+    printf("NOT: %d\n", !(a > 5));
     return 0;
 }
